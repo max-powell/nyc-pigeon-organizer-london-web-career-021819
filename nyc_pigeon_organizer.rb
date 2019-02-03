65ac055c4e3e94da98621b37b51115e299da7d8f
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-03 12:54:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #set up new hash
+  new_hash = {}
+  pigeons = []
+  data[:gender].each {|k, v| pigeons << v}
+  pigeons.flatten!
+  pigeons.each do |pigeon|
+    new_hash[pigeon] = {
+      color: [],
+      gender: [],
+      lives: []
+    }
+  end
+  # input data to new hash
+  data.each do |trait, options|
+    options.each do |option, pigeons|
+      pigeons.each do |pigeon|
+        new_hash[pigeon][trait] << "#{option}"
+      end
+    end
+  end
+  new_hash
 end
